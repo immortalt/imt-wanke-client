452d173a9c6b7ff44c86eeec9685e9288037cb82
Immortalt <tianqihang@126.com>
2017-10-20 16:30:48 +0800

    防止重复运行

--- imt_wankeyun_client/App.xaml.cs
+++ imt_wankeyun_client/App.xaml.cs
@@ -12,5 +12,21 @@ namespace imt_wankeyun_client
     /// </summary>
     public partial class App : Application
     {
+        System.Threading.Mutex mutex;
+        public App()
+        {
+            this.Startup += new StartupEventHandler(App_Startup);
+        }
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            bool ret;
+            mutex = new System.Threading.Mutex(true, "imt-wanke-client", out ret);
+            if (!ret)
+            {
+                MessageBox.Show("已有一个程序实例运行", "提示");
+                Environment.Exit(0);
+            }
+
+        }
     }
 }
