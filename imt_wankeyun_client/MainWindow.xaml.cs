c9238f4f05b539db9e749215e60ce3dcf82eabe3
Immortalt <tianqihang@126.com>
2017-11-05 00:24:25 +0800

    实现支持窗体自由缩放

--- imt_wankeyun_client/MainWindow.xaml.cs
+++ imt_wankeyun_client/MainWindow.xaml.cs
@@ -27,6 +27,9 @@ using imt_wankeyun_client.Entities.Control.RemoteDL;
 using System.Windows.Media.Imaging;
 using System.Deployment.Application;
 using Microsoft.VisualBasic;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using System.Windows.Media;
 
 namespace imt_wankeyun_client
 {
@@ -55,6 +58,12 @@ namespace imt_wankeyun_client
         public MainWindow()
         {
             InitializeComponent();
+            this.SourceInitialized += delegate (object sender, EventArgs e)//执行拖拽
+            {
+                this._HwndSource = PresentationSource.FromVisual((Visual)sender) as HwndSource;
+            };
+            this.MouseMove += new MouseEventHandler(Window_MouseMove);//鼠标移入到边缘收缩
+
             onlineIcon = Properties.Resources.online;
             offlineIcon = Properties.Resources.offline;
             this.notifyIcon = new System.Windows.Forms.NotifyIcon();
@@ -76,6 +85,7 @@ namespace imt_wankeyun_client
             {
                 if (e.Button == System.Windows.Forms.MouseButtons.Left) this.Show(o, e);
             });
+
             try
             {
                 tbk_version.Text = GetEdition();
@@ -1554,6 +1564,42 @@ namespace imt_wankeyun_client
                 MessageBox.Show(resp == "success" ? "安全弹出硬盘成功！可以拔掉硬盘了" : resp, "提示");
                 await RefreshStatus(phone);
             }
+        }
+        #region 初始化窗体可以缩放大小
+        private const int WM_SYSCOMMAND = 0x112;
+        private HwndSource _HwndSource;
+        private Dictionary<ResizeDirection, Cursor> cursors = new Dictionary<ResizeDirection, Cursor>
+        {
+            {ResizeDirection.BottomRight, Cursors.SizeNWSE},
+        };
+        private enum ResizeDirection
+        {
+            BottomRight = 8,
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+        #endregion
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Mouse.LeftButton != MouseButtonState.Pressed)
+            {
+                FrameworkElement element = e.OriginalSource as FrameworkElement;
+                if (element != null && !element.Name.Contains("Resize"))
+                    this.Cursor = Cursors.Arrow;
+            }
+        }
+        private void ResizePressed(object sender, MouseEventArgs e)
+        {
+            FrameworkElement element = sender as FrameworkElement;
+            ResizeDirection direction = (ResizeDirection)Enum.Parse(typeof(ResizeDirection), element.Name.Replace("Resize", ""));
+            this.Cursor = cursors[direction];
+            if (e.LeftButton == MouseButtonState.Pressed)
+                ResizeWindow(direction);
+        }
+        private void ResizeWindow(ResizeDirection direction)
+        {
+            SendMessage(_HwndSource.Handle, WM_SYSCOMMAND, (IntPtr)(61440 + direction), IntPtr.Zero);
         }
     }
 }
